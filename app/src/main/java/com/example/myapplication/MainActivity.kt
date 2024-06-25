149b46353b52875e9c844c2758d6d9d1049dc3e2
trdung245 <albienguyen245@gmail.com>
2024-06-25 21:04:56 +0700

    + Register

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var usernameInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var loginButton: Button
+    private lateinit var registerButton1: Button
+    private lateinit var registerButton2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +24,20 @@ class MainActivity : AppCompatActivity() {
         usernameInput = findViewById(R.id.username_input)
         passwordInput = findViewById(R.id.password_input)
         loginButton = findViewById(R.id.loginbtn)
+        registerButton1 = findViewById(R.id.registerbtn1)
+
+        registerButton1.setOnClickListener{
+            setContentView(R.layout.register)
+
+            registerButton2 = findViewById(R.id.registerbtn2)
+            registerButton2.setOnClickListener{
+                val username = usernameInput.text.toString()
+                val password = passwordInput.text.toString()
+
+                val intent = Intent(this, MainMenuActivity::class.java)
+                startActivity(intent)
+            }
+        }
 
         loginButton.setOnClickListener {
             val username = usernameInput.text.toString()
