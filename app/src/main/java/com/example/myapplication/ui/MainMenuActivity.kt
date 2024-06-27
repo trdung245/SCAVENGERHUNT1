85d3936a028464f7d955be9ea3eb1329825ba302
trdung245 <albienguyen245@gmail.com>
2024-06-27 22:44:10 +0700

    find game -> event

--- app/src/main/java/com/example/myapplication/ui/MainMenuActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainMenuActivity.kt
@@ -10,7 +10,7 @@ import com.example.myapplication.R
 class MainMenuActivity : AppCompatActivity() {
 
     private lateinit var captureButton: ImageButton
-    private lateinit var findgameButton: ImageButton
+    private lateinit var eventButton: ImageButton
     private lateinit var balanceButton: ImageButton
     private lateinit var profileButton: ImageButton
     private lateinit var communityButton: ImageButton
@@ -22,7 +22,7 @@ class MainMenuActivity : AppCompatActivity() {
         setContentView(R.layout.main_menu)
 
         captureButton = findViewById(R.id.capture_button)
-        findgameButton = findViewById(R.id.find_game_button)
+        eventButton = findViewById(R.id.event_button)
         balanceButton = findViewById(R.id.balance_button)
         profileButton = findViewById(R.id.profile_button)
         communityButton = findViewById(R.id.community_button)
@@ -30,6 +30,11 @@ class MainMenuActivity : AppCompatActivity() {
 
         captureButton.setOnClickListener{
             val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        eventButton.setOnClickListener{
+            val intent = Intent(this, EventActivity::class.java)
             startActivity(intent)
         }
 
